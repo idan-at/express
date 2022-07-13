4dd40d08b9013735dfdd4a9b7f75d2e9c99477ea
Idan Attias <idana@wix.com>
2022-07-13 21:24:27 +0300

    proper chain

--- src/main/java/com/express/IncomingHandler.java
+++ src/main/java/com/express/IncomingHandler.java
@@ -4,33 +4,34 @@ import com.sun.net.httpserver.HttpExchange;
 
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Iterator;
 
 class IncomingHandler {
     private final ArrayList<HandlerContainer> handlerContainers = new ArrayList<>();
 
-    public void add(HandlerContainer handlerContainer) {
+    void add(HandlerContainer handlerContainer) {
         handlerContainers.add(handlerContainer);
     }
 
-    public void handle(HttpExchange exchange) {
+    void handle(HttpExchange exchange) {
         final Request request = new Request();
         final Response response = new Response(exchange);
 
-        final List<Handler> handlers = buildHandlersChain(exchange);
-        final NextFunctionHandler next = new NextFunctionHandlerImpl();
+        final Iterator<Handler> handlers = buildHandlersChain(exchange);
 
-        handlers.forEach(handler -> handler.handle(request, response, next));
+        final Handler first = handlers.next();
+        final NextFunctionHandler next = new NextFunctionHandlerImpl(request, response, handlers);
+
+        first.handle(request, response, next);
     }
 
-    private List<Handler> buildHandlersChain(HttpExchange exchange) {
+    private Iterator<Handler> buildHandlersChain(HttpExchange exchange) {
         final String method = exchange.getRequestMethod();
         final URI url = exchange.getRequestURI();
 
         return handlerContainers.stream()
                 .filter(handlerContainer -> handlerContainer.matches(method, url))
                 .map(HandlerContainer::getHandler)
-                .collect(Collectors.toList());
+                .iterator();
     }
 }

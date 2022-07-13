efcd6412fbd239ad2f735faf1679013b1ff33c3d
Idan Attias <idana@wix.com>
2022-07-13 21:26:17 +0300

    stricter accesstors

--- src/main/java/com/express/HandlerContainer.java
+++ src/main/java/com/express/HandlerContainer.java
@@ -7,17 +7,17 @@ class HandlerContainer {
     private final String pattern;
     private final Handler handler;
 
-    public HandlerContainer(HttpMethod method, String pattern, Handler handler) {
+    HandlerContainer(HttpMethod method, String pattern, Handler handler) {
         this.method = method;
         this.pattern = pattern;
         this.handler = handler;
     }
 
-    public boolean matches(String method, URI uri) {
+    boolean matches(String method, URI uri) {
         return this.method.toString().equals(method) && pattern.equals(uri.toString());
     }
 
-    public Handler getHandler() {
+    Handler getHandler() {
         return handler;
     }
 }
